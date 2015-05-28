d038ce8591385de43418c6ed47da26a0731ce575
Rob Colburn <rob@robcolburn.com>
2015-05-28 15:58:30 -0700

    Simplify test script

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var mochaPhantomJS = require('gulp-mocha-phantomjs');
- 
-gulp.task('test', function () {
-    return gulp
-    .src('test/runner.html')
-    .pipe(mochaPhantomJS());
-});
